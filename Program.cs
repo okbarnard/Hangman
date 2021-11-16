d4443409af98266e04e49b922cf565190f622585
okbarnard <contact.kbarnard@gmail.com>
2021-11-16 13:59:33 -0800

    added difficulty options and method to reduce redundancy in display. Additional comments for plans, adjusted grouping for cohesiveness and clarity.

--- Program.cs
+++ Program.cs
@@ -8,34 +8,31 @@ namespace Hangman
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hangman!");
-            //Add letters guessed
-            string[] wordList = {
-                "computer",
-                "waterfountain",
-                "sunglasses",
-                "interview",
-                "national",
-                "microphone",
-                "vitamins",
-                "waterbottle",
-                "parkinglot",
-                "spaceship"
-            };
-
-            //variable for correct word, guessed word, and guesses
-            Random rnd = new Random();
-            int num = rnd.Next(0, 9);
-            string correctWord = wordList[num];
-            List<char> guesses = new List<char>();
-            char[] placeholderDisplay = new char[correctWord.Length];
+            //difficulty selection
+            int difficulty = 0;
+            do
+            {
+                Console.WriteLine("Please select your difficulty level: \n1 - Easy\n2 - Medium\n3 - Hard");
+                difficulty = Convert.ToInt32(Console.ReadLine());
+                if (difficulty < 1 || difficulty > 3)
+                {
+                    Console.WriteLine("Please enter a number of 1 - 3 to select your difficulty.");
+                }
+            }
+            while (difficulty < 1 || difficulty > 3);
             
-            //setting default values as '_' in placeholderDisplay
+            //select word according to difficulty
+            string correctWord = WordDifficulty(difficulty);
+
+            //set default values as '_' in placeholderDisplay to show user
+            char[] placeholderDisplay = new char[correctWord.Length];
             for (int i = 0; i < correctWord.Length; i++)
             {
                 placeholderDisplay[i] = '_';
             }
 
-            //guesses
+            //guess & strike declaration
+            List<char> guesses = new List<char>();
             int strikes = 0;
             char guess;
 
@@ -68,28 +65,17 @@ namespace Hangman
                         break;
                 }
 
-                //display previous guesses
+                //display guesses (correct & incorrect)
                 Console.Write("Previous Guesses: ");
-                foreach (var letter in guesses)
-                {
-                    Console.Write("{0}", letter);
-                }
-
-                Console.WriteLine("\n");
-
-                //display placeholders/correct guesses
-                foreach (var letter in placeholderDisplay)
-                {
-                    Console.Write("{0 }", letter);
-                }
-                Console.WriteLine("\n");
+                Display(guesses.ToArray());
+                Display(placeholderDisplay);
 
                 //Guess prompt, intake, and store
                 Console.Write("Guess a letter: ");
                 guess = Convert.ToChar(Console.ReadLine());
                 guesses.Add(guess);
 
-                //word length display
+                //if guess is correct, update placeholderDisplay
                 for (int i = 0; i < correctWord.Length; i++)
                 {
                     if (guess == correctWord[i])
@@ -97,7 +83,8 @@ namespace Hangman
                         placeholderDisplay[i] = guess;
                     }
                 }
-                //strike if incorrect guess
+
+                //if guess is incorrect, increase strikes
                 if (!(correctWord.Contains(guess)))
                 {
                     strikes++;
@@ -125,14 +112,95 @@ namespace Hangman
                 Console.WriteLine("You've been hung! ): ): ");
             }
         }
+
+        private static string WordDifficulty(int difficulty)
+        {
+            string word = "";
+            Random rnd = new Random();
+            int num = rnd.Next(0, 9);
+
+            //change from array to a class & list for hints for each word and customization in two players
+            string[] easyWordList =
+            {
+                "dessert",
+                "peace", //replace...?
+                "tennis",
+                "strike",
+                "distress",
+                "telephone",
+                "kitchen",
+                "outside",
+                "aoili",
+                "deodorant"
+            };
+
+            string[] mediumWordList =
+            {
+                "computer",
+                "stretch",
+                "sunglasses",
+                "kitchen",
+                "national",
+                "basketball",
+                "equip",
+                "pajama",
+                "avenue",
+                "deodorant"
+            };
+
+            string[] hardWordList =
+            {
+                "beekeeper",
+                "awkward",
+                "interview",
+                "pneumonia",
+                "onyx",
+                "disavow",
+                "kiosk",
+                "jackpot",
+                "galaxy",
+                "vitamins"
+            };
+
+            if (difficulty == 1)
+            {
+                word = easyWordList[num];
+            }
+            else if (difficulty == 2)
+            {
+                word = mediumWordList[num];
+            }
+            else
+            {
+                word = hardWordList[num];
+            }
+
+            return word;
+        }
+        private static void Display(char[] displayArr)
+        {
+            foreach (var letter in displayArr)
+            {
+                Console.Write("{0 }", letter);
+            }
+
+            Console.WriteLine("\n");
+        }
     }
 }
 
-//Single or multiplayer
-// *Ask for how many rounds (provide options --3, 5, or 7, where last of each round WHEN the score is even is "Tiebreaker!!!" )
-// *Option to provide the word (with a character limit, maybe 20) Supercalifragilisticexpialidocious, no numbers or special characters
-// *add hints later on? -- add option to add hints if providing the word. Asking for a hint also adds a limb to the hangman. Add a warning when they only have 2 limbs left and they can't ask for a hint on the last... leg (limb)
-// * Random generator for what hint to choose to display; 3 hints max.
-// *If single player and no hangman "graphic," displays how many turns they have left (head, body, arm, arm, leg, leg --total of 6 guesses).
-// *Difficulty level of words?
-// Add letters guessed.
+/*
+ * multiplayer
+ *specify whose turn it is
+ *ask for how many rounds (provide options --3, 5, or 7, where last of each round when the score is even is "tiebreaker!!!" )
+ *option to provide the word (with a character limit, maybe 20) supercalifragilisticexpialidocious, no numbers or special characters
+ *random generator for what hint to choose to display; 3 hints max.
+ *two players will need the difficulty choices individually?
+ *check for numbers in word
+ *
+ *general notes
+ *add hints later on? -- add option to add hints if providing the word. asking for a hint also adds a limb to the hangman. add a warning when they only have 2 limbs left and they can't ask for a hint on the last... leg (limb)
+ *add option to exit game?
+ *add catch if user enters a number
+ *add catch if user enters a word -- probably convert to string so if they guess the word...?
+ */
