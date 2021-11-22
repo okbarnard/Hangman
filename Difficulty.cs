59d3cba33c99e1bf73529ba4fa4aebe936802bac
okbarnard <contact.kbarnard@gmail.com>
2021-11-22 08:25:59 -0800

    created one player, separated classes, added enum for difficulty (still need to execute).

---
+++ Difficulty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    enum Difficulty
+    {
+        Easy = 1,
+        Medium = 2,
+        Hard = 3
+
+    }
+}
